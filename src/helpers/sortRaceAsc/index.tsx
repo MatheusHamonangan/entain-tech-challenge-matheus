07d1f02cdd9a68b53958843b6dc2dae7d7f6d7d9
Matheus Hamonangan <matheusgultom@gmail.com>
2021-08-30 20:10:21 +1000

    Update func comment

--- src/helpers/sortRaceAsc/index.tsx
+++ src/helpers/sortRaceAsc/index.tsx
@@ -1,3 +1,7 @@
+/**
+ * This function sorts the race list asc based on the
+ * advertised_date attribute
+ */
 const sortRaceAsc = (values: any): Array<Object> => {
     return values.sort(function (a: {
         advertised_start: {seconds: number}
